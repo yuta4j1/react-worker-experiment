da8cdeb6eea64fbea7794f8cc30b2da6215682c3
yuta4j1 <kascado.ys10@gmail.com>
2021-01-09 21:57:14 +0900

    update

--- src/worker/service-worker.ts
+++ src/worker/service-worker.ts
@@ -1,6 +1,6 @@
 const CACHE_NAME = "cache-v1"
 const urlsToCache = ["/", "/styles/main.css", "/script/main.js"]
-self.addEventListener("install", e => {
+self.addEventListener("install", (e: any) => {
   console.log("install Event")
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
