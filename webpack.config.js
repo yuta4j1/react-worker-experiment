a834be6e9cdd0fe9f85994c55d8e9b20c6748f09
yuta4j1 <kascado.ys10@gmail.com>
2021-07-22 13:10:17 +0900

    フォーマット

--- webpack.config.js
+++ webpack.config.js
@@ -3,36 +3,36 @@ const HtmlWebpackPlugin = require("html-webpack-plugin")
 const { CleanWebpackPlugin } = require("clean-webpack-plugin")
 
 module.exports = {
-    entry: {
-        app: "./src/index.tsx"
-    },
-    output: {
-        filename: "[name].js",
-        path: path.resolve(__dirname, "dist")
-    },
-    module: {
-        rules: [
-            {
-                test: /\.tsx?$/,
-                use: ["ts-loader"],
-                exclude: /node_modules/
-            }
-        ]
-    },
-    resolve: {
-        extensions: [".js", ".ts", ".tsx"]
-    },
-    plugins: [
-        new CleanWebpackPlugin(),
-        new HtmlWebpackPlugin({
-            template: "./template/index.html",
-            minify: false,
-        })
+  entry: {
+    app: "./src/index.tsx",
+  },
+  output: {
+    filename: "[name].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: ["ts-loader"],
+        exclude: /node_modules/,
+      },
     ],
-    devServer: {
-        contentBase: path.join(__dirname, 'dist'),
-        hot: true,
-        open: true,
-        port: 9000
-      }
+  },
+  resolve: {
+    extensions: [".js", ".ts", ".tsx"],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./template/index.html",
+      minify: false,
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "dist"),
+    hot: true,
+    open: true,
+    port: 9000,
+  },
 }
